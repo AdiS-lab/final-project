eaa2e0067882f5963ded17858fc478aea06dbf9f
Adi Shankar <adishankar219@gmail.com>
2026-06-07 23:10:15 -0700

    more little changes

--- frontend/src/Pages/Gallery.tsx
+++ frontend/src/Pages/Gallery.tsx
@@ -5,10 +5,9 @@ type imgArr = {
     imgArr:{imgUrl: string, imgId: string, imgName: string}[]
     handleClick: (id:string)=>void
     deleteSession: (id: string) =>void
-    deleteLoading: Boolean
 }
 
-const Gallery = ({imgArr, handleClick, deleteSession, deleteLoading}:imgArr)=>{
+const Gallery = ({imgArr, handleClick, deleteSession}:imgArr)=>{
     return(
         <div className='min-h-full w-full flex flex-col overflow-y-auto' style={{ background: '#1a1a1a' }}>
             <div className='px-8 py-6' style={{ borderBottom: '1px solid #2a2a2a' }}>
@@ -16,7 +15,7 @@ const Gallery = ({imgArr, handleClick, deleteSession, deleteLoading}:imgArr)=>{
             </div>
             <div className='p-8 w-full flex gap-6 flex-wrap'>
                 {imgArr.map((img: {imgUrl: string, imgId: string, imgName: string }, index: number)=>(
-                    img.imgUrl && <CardsGallery key = {'card' + index} previewImage={img.imgUrl} name={img.imgName} deleteSession = {()=>{deleteSession(img.imgId)}} onClick = {()=>handleClick(img.imgId)} deleteLoading = {deleteLoading}/>
+                    img.imgUrl && <CardsGallery key = {'card' + index} previewImage={img.imgUrl} name={img.imgName} deleteSession = {()=>{deleteSession(img.imgId)}} onClick = {()=>handleClick(img.imgId)}/>
                 ))
                 }
             </div>
